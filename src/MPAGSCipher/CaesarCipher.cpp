c73737d76c783d2fbc66af61cd7aa5df43adb425
Chandler Kenworthy <chandlerkenworthy@gmail.com>
2022-11-04 14:17:33 +0000

    Move cipher implementation into CaesarCipher class

--- src/MPAGSCipher/CaesarCipher.cpp
+++ src/MPAGSCipher/CaesarCipher.cpp
@@ -1,7 +1,51 @@
 #include "CaesarCipher.hpp"
+#include <string>
+#include <iostream>
 
-CaesarCipher::CaesarCipher(const int key)
+CaesarCipher::CaesarCipher(const std::size_t key)
     : key_{key} 
 {
     // some stuff goes here
+}
+
+CaesarCipher::CaesarCipher(const std::string key) : key_{0} {
+    for (const auto& elem : key) {
+        if (!std::isdigit(elem)) {
+            return;
+        }
+    }
+    key_ = std::stoul(key);
+}
+
+std::string CaesarCipher::applyCipher(const bool encrypt, const std::string inputText) {
+    // Create the output string
+    std::string outputText;
+
+    // Define size of the alphabet
+    const std::size_t alphabetSize{alphabet_.size()};
+
+    // Make sure that the key is in the range 0 - 25
+    const std::size_t truncatedKey{key_ % alphabetSize};
+
+    // Loop over the input text
+    char processedChar{'x'};
+    for (const auto& origChar : inputText) {
+        for (size_t i{0}; i < alphabetSize; ++i) {
+            if (origChar == alphabet_[i]) {
+                // Apply the appropriate shift (depending on whether we're encrypting
+                // or decrypting) and determine the new character
+                // Can then break out of the loop over the alphabet
+                if (encrypt) {
+                    processedChar = alphabet_[(i + truncatedKey) % alphabetSize];
+                } else {
+                    processedChar = alphabet_[(i + alphabetSize - truncatedKey) %
+                                             alphabetSize];
+                }
+                break;
+            }
+        }
+        // Add the new character to the output text
+        outputText += processedChar;
+    }
+    return outputText;
 }
--- src/MPAGSCipher/CaesarCipher.hpp
+++ src/MPAGSCipher/CaesarCipher.hpp
@@ -1,10 +1,18 @@
 #ifndef MPAGSCIPHER_CAESARCIPHER_HPP
 #define MPAGSCIPHER_CAESARCIPHER_HPP
+#include <string>
+#include <vector>
 
 class CaesarCipher {
     public:
-        CaesarCipher(const int key);
-        int key_; 
+        explicit CaesarCipher(const std::size_t key);
+        explicit CaesarCipher(const std::string key);
+        std::string applyCipher(const bool encrypt, const std::string inputText);
+    private:
+        std::size_t key_; 
+        const std::vector<char> alphabet_ = {
+        'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
+        'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
 };
 
 #endif
